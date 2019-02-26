9970be8dcd6c925c1e55bb1ec49f85ff1d0ad2d3
giokalm <giorgoskalmoukis@outlook.com>
2019-02-26 15:07:15 +0200

    Clean unused code

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -14,6 +14,21 @@ export class EmployeeDetailsComponent implements OnInit {
     this.id = route.snapshot.params['id'];
   }
 
+  role_string(id){
+    // iterate over each element in the array
+    for (var i = 0; i < this.roles.length; i++){
+      // look for the entry with a matching `code` value
+      if (this.roles[i].value == id){
+        // we found it
+        return this.roles[i].string;
+      }
+    }
+  }
+ 
+
+
+  roles = [{"value":"1","string":"Nurse"}, {"value":"2","string":"Supervisor"}];
+
   ngOnInit() {
     this._api.getEmployee(this.id)
     .subscribe(data => {
--- src/app/schedules/schedules.component.ts
+++ src/app/schedules/schedules.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchedulesComponent implements OnInit {
 
-  schedules = [
-    { id: 11,  weeks:"3" ,employees:"4" }
-  ];
+  schedules = [];
 
   constructor() { }
 
